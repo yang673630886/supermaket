bb490a1fdbe4c40641b2f070314ee84c949a5930
yangjingsheng <673630886@qq.com>
2020-07-30 09:56:48 +0800

    计算类 Calulator add 计算价格 calculatePrice add 计算促销价格 calculateSalePrice add 计算非促销价格 calculateNotSalePrice

--- src/main/java/com/thoughtworks/supermarket/Calulator.java
+++ src/main/java/com/thoughtworks/supermarket/Calulator.java
@@ -4,5 +4,34 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class Calulator {
+    BigDecimal calculatePrice(List<Goods> goodsList) {
+        BigDecimal sum = BigDecimal.ZERO;
+        for (Goods goods : goodsList
+        ) {
+            sum = sum.add(goods.getPrice());
+        }
+        return sum;
+    }
+
+    BigDecimal calculateSalePrice(List<Goods> goodsList) {
+        BigDecimal saleprice = BigDecimal.ZERO;
+        for (Goods goods : goodsList) {
+            if (!goods.notSale(goods.getGoodsType())) {
+                saleprice = saleprice.add(goods.getPrice());
+            }
+        }
+        return saleprice;
+    }
+
+    BigDecimal calculateNotSalePrice(List<Goods> goodsList) {
+        BigDecimal notsaleprice = BigDecimal.ZERO;
+        for (Goods goods : goodsList) {
+            if (goods.notSale(goods.getGoodsType())) {
+                notsaleprice = notsaleprice.add(goods.getPrice());
+            }
+        }
+        return notsaleprice;
+
+    }
 
 }
