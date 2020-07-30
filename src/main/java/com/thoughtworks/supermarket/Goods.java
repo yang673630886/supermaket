951bcd0dd1cc6982104a239c715a8901a2b6a4ee
yangjingsheng <673630886@qq.com>
2020-07-30 09:54:01 +0800

    商品类 Goods

--- src/main/java/com/thoughtworks/supermarket/Goods.java
+++ src/main/java/com/thoughtworks/supermarket/Goods.java
@@ -3,4 +3,41 @@ package com.thoughtworks.supermarket;
 import java.math.BigDecimal;
 
 public class Goods {
+    private String name;
+    private BigDecimal price;
+    private GoodsType goodsType;
+
+    public Goods(String name, BigDecimal price, GoodsType goodsType) {
+        this.name = name;
+        this.price = price;
+        this.goodsType = goodsType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public GoodsType getGoodsType() {
+        return goodsType;
+    }
+
+    public void setGoodsType(GoodsType goodsType) {
+        this.goodsType = goodsType;
+    }
+
+    public boolean notSale(GoodsType goodsType){
+        return goodsType.equals(GoodsType.OTHER);
+    }
 }
