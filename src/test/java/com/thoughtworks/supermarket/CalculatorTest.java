2542bd2e633a133d34eca0c4cdfc3ae0f65c5971
yangjingsheng <673630886@qq.com>
2020-07-30 09:52:23 +0800

    should_return_score_1_when_calculate_given_price_1 1块钱1积分

--- src/test/java/com/thoughtworks/supermarket/CalculatorTest.java
+++ src/test/java/com/thoughtworks/supermarket/CalculatorTest.java
@@ -12,6 +12,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
+    @Test
+    public void should_return_score_1_when_calculate_given_price_1() {
+        Goods goods = new Goods("pen", BigDecimal.valueOf(1), GoodsType.OTHER);
+        List<Goods> goodsList = Arrays.asList(goods);
+        Calulator calulator = new Calulator();
+        int result = calulator.calculatePrice(goodsList).intValue();
+        assertEquals(1, result);
+    }
 
 
 }
