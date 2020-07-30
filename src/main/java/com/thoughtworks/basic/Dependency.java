05c4b2bc9bc0aaca4f91baed85f9d1407e76814d
yangjingsheng <673630886@qq.com>
2020-07-30 09:42:10 +0800

    add

--- src/main/java/com/thoughtworks/basic/Dependency.java
+++
@@ -1,7 +0,0 @@
-package com.thoughtworks.basic;
-
-public class Dependency {
-    public String say(){
-        return "Leave me alone.";
-    }
-}
---
+++ src/main/java/com/thoughtworks/supermarket/GoodsType.java
@@ -0,0 +1,6 @@
+package com.thoughtworks.supermarket;
+
+public enum GoodsType {
+    APPLE,WATERMELON,BEEF,TV,ICEBOX,OTHER
+}
+
